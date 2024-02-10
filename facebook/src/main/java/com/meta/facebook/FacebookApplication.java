2d807a01836209f19e2c4bc911e36536d7c84e72
abdulahadmalik1 <159634565+abdulahadmalik1@users.noreply.github.com>
2024-02-10 08:41:44 -0800

    Add files via upload

---
+++ facebook/src/main/java/com/meta/facebook/FacebookApplication.java
@@ -0,0 +1,14 @@
+package com.meta.facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookApplication {
+
+	public static void main(String[] args) {
+		// Run the Spring application and get the application context
+		var a= SpringApplication.run(FacebookApplication.class, args);
+		var a2=a.getBean(check.class);
+	}
+}
